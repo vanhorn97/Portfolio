21d1d834f0090fb17b6374951981dff67c1daab3
vanhorn97 <michaelvanhorn97@gmail.com>
2025-08-20 14:04:54 -0400

    Adding styles

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 const navbarToggle = document.querySelector('.navbar-toggle');
 const navbarMenu = document.querySelector('.navbar-menu');
 const overlay = document.querySelector('.overlay');
-const navbar = document.querySelector('.navbar');
-
-navbar.addEventListener('scroll', () => {
-    if (window.scrollY > 0) {
-        navbar.classList.add('.scrolled');
-     } else {
-        navbar.classList.remove('.scrolled');
-    }
-});
 
 navbarToggle.addEventListener('click', () => {
     navbarToggle.classList.toggle('active');
