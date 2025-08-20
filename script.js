9b77e73a80b840bdfcac8d6f16b7b33bde8c2da0
vanhorn97 <michaelvanhorn72@gmail.com>
2025-08-19 23:14:56 -0400

    Major changes to the portfolio website

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const navbarToggle = document.querySelector('.navbar-toggle');
 const navbarMenu = document.querySelector('.navbar-menu');
+const overlay = document.querySelector('.overlay');
 
 navbarToggle.addEventListener('click', () => {
     navbarToggle.classList.toggle('active');
     navbarMenu.classList.toggle('active');
+    overlay.classList.toggle('active');
+});
+
+overlay.addEventListener('click', () => {
+    navbarToggle.classList.remove('active');
+    navbarMenu.classList.remove('active');
+    overlay.classList.remove('active');
 });
