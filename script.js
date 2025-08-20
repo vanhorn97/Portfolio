f5c4db0fcb1e658d7279364b303fcb8df75eb028
vanhorn97 <michaelvanhorn72@gmail.com>
2025-08-19 20:32:16 -0400

    Starting from scratch on styling and functionality

--- script.js
+++ script.js
@@ -1,38 +1,7 @@
-const openButton = document.querySelector('.open-sidebar');
-const navbar = document.getElementById('navbar');
+const navbarToggle = document.querySelector('.navbar-toggle');
+const navbarMenu = document.querySelector('.navbar-menu');
 
-const media = window.matchMedia('(width < 768px)')
-
-media.addEventListener('change', (e) => updateNavbar(e));
-
-function updateNavbar(e){
-    const isMobile = e.matches;
-    console.log(isMobile)
-    if(isMobile){
-        navbar.setAttribute('inert, "')
-    }
-    else{
-        navbar.removeAttribute('inert');
-    }   
-}
-
-function openSidebar() {
-    navbar.classList.add('show');
-    openButton.setAttribute('aria-expanded', 'true');
-    navbar.removeAttribute('inert');
-}
-
-function closeSidebar() {
-    navbar.classList.remove('show');
-    openButton.setAttribute('aria-expanded', 'false');
-    navbar.setAttribute('inert', '');
-}
-
-const navLinks = document.querySelectorAll('nav a')
-navLinks.forEach(Link => {
-    Link.addEventListener('click', () => {
-        closeSidebar()
-    })
-})
-
-updateNavbar(media)
+navbarToggle.addEventListener('click', () => {
+    navbarToggle.classList.toggle('active');
+    navbarMenu.classList.toggle('active');
+});
